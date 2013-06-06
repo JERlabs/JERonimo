e20c01fa5505ca2352b1aa5d0bcbe80b06840c19
Jacob Hughes <exrook.j@gmail.com>
2013-06-06 17:28:13 -0400

    Removed extraneous functions that can be replaced by casts from Point.h

--- Point.h
+++ Point.h
@@ -6,25 +6,20 @@ class Point {
     long double Y;
   public:
     Point (long double, long double);
-    Point (int, int);
     long double x();
     long double x(long double);
-    int x(int);
     long double y();
     long double y(long double);
-    int y(int);
     Point operator + (Point);
     Point operator - (Point);
     Point operator * (long double);
-    Point operator * (int);
     Point operator / (long double);
-    Point operator / (int);
     Point operator += (Point);
     Point operator -= (Point);
     Point operator *= (long double);
-    Point operator *= (int);
     Point operator /= (long double);
-    Point operator /= (int);
+    Point operator == (Point);
+    Point operator != (Point);
 };
 
 }
