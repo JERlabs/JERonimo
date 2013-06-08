655182c74ca85c5ddc0f9d07edd218ab16689c96
EdWard680 <ezeward4@gmail.com>
2013-06-08 19:59:10 -0400

    Adds test header file since tests use similar methods

--- test/Space2D/PointTest.cpp
+++ test/Space2D/PointTest.cpp
@@ -1,17 +1,5 @@
-#include <iostream>
-#include "Point.h"
-using namespace Space2D;
+#include "Space2dTest.h"
 
-Point makePoint(long double const &x, long double const &y, char const *name) {
-  Point lol = Point(x,y);
-  std::cout << "created point " << name << "(" << lol.x() << "," 
-  << lol.y() << ")\n";
-  return lol;
-}
-void printPoint(char const *name, Point const &point) {
-  std::cout << "Point " << name << " has value: (" << point.x() << ","
-  << point.y() << ")\n";
-}
 int main(int argc, char *argv[]) {
   Point a = makePoint(4,4, "a");
   std::cout << "Multiplying a by 5 \n";
---
+++ test/Space2D/Space2DTest.h
@@ -0,0 +1,36 @@
+#ifndef _SPACE2D_TEST_H_
+#define _SPACE2D_TEST_H_
+
+#include <iostream>
+
+#include "Point.h"
+#include "Vector.h"
+
+using namespace Space2D;
+using std::cout;
+using std::endl;
+
+#define PRINT_POINT(p) printPoint(#p, (p))
+#define PRINT_VECTOR(v) printVector(#v, (v))
+
+const Vector makeVector(const long double &m, const long double &t, const char * const name) {
+  cout<<"Created vector "<<name<<", "<<m<<"<"<<t<<" rad"<<endl;
+  return Vector(m, t);
+}
+
+void printVector(const char * const name, const Vector &V) {
+  cout<<"Vector "<<name<<" has a magnitude of "<<V.mag()<<" at "<<V.theta()<<" rad"<<endl;
+}
+
+Point makePoint(long double const &x, long double const &y, char const *name) {
+  Point lol = Point(x,y);
+  cout << "created point " << name << "(" << lol.x() << ", " 
+  << lol.y() << ")\n";
+  return lol;
+}
+void printPoint(char const *name, Point const &point) {
+  cout << "Point " << name << " has value: (" << point.x() << ", "
+  << point.y() << ")\n";
+}
+
+#endif /*SPACE2D_TEST_H_*/
