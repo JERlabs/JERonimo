e703b2d1501f33d94ca9d7384d93961c9937c17c
EdWard680 <ezeward4@gmail.com>
2013-06-10 16:04:14 -0400

    weird git behavior. Troubleshooting commits

--- GameControl/GameControl.h
+++ GameControl/GameControl.h
@@ -8,9 +8,11 @@
 #include "Velocity.h"
 
 namespace GameControl {
-  class Events;   //handles events and allows GameLoop to inherit every event as a overloadable function
-  class GameLoop; //inherites Events and handles the control flow. Classes which inherit overload the various parts of its standard loop
-  class App;      //controls which gameloop is being run, initializes SDL
+  class Events;   /// handles events and allows GameLoop to inherit every event as an overloadable function
+  class GameLoop; /// inherites Events and handles the control flow. Classes which inherit overload the various parts of its standard loop
+  class App;      /// controls which gameloop is being run, initializes SDL
+  //This comment is here for no purpose. Recent git push was unsuccessful, so by making this change, it will have to push changes again,
+  //without claiming things are up to date.
 
 }
 
