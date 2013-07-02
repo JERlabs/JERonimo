d545aeb64c1ac68962720b06dad6fc2769ea4bbc
exrook <exrook.j@gmail.com>
2013-07-02 14:04:17 -0400

    Added and fixed documentation for JustASurf and OpenSurf

--- SurfaceX/OpenSurf.h
+++ SurfaceX/OpenSurf.h
@@ -10,14 +10,21 @@ private:
   SDL_Surface *canvas;
   
 public:
+  /// Return the internal raw surface
   virtual SDL_Surface * const Surf() const {return canvas;};
+  /// Set the internal raw surface to set
   virtual SDL_Surface * const Surf(SDL_Surface * const set) {return canvas = set;};
+  /// Return the internal raw surface
   virtual operator SDL_Surface *() const {return Surf();};
   
 public:
+  /// Create a new OpenSurf with the internal surface set to NULL
   OpenSurf(): canvas(NULL) {};
+  /// Create a new OpenSurf with the internal surface set to init
   OpenSurf(SDL_Surface * const init): canvas(init) {canvas->refcount++;};
+  /// Create a new OpenSurf with an image loaded from file
   OpenSurf(char * const file): JustASurf(file) {};
+  /// Destroy this OpenSurf, frees the internal surface as well
   virtual ~OpenSurf();
 };
 
