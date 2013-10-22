29ead6e4789b33139d48d22bfa74b75c7bd0ae39
EdWard680 <ezeward4@gmail.com>
2013-10-22 17:34:11 -0400

    Fixed Fucking 4

--- Space2D/Space2D.cpp
+++ Space2D/Space2D.cpp
@@ -11,7 +11,7 @@ const Degrees Degrees::ANGLE_LEFT(180.0);
 const Degrees Degrees::ANGLE_DOWN(270.0);
 
 const Radians Radians::ANGLE_RIGHT(0.0);
-const Radians Radians::ANGLE_UP(TAO/4);
+const Radians Radians::ANGLE_UP(TAO/4.0);
 const Radians Radians::ANGLE_LEFT(TAO/2.0);
 const Radians Radians::ANGLE_DOWN(3.0*TAO/4.0);
 
