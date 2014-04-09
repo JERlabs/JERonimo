561a24ed859f3fecab0761e510772647d159b47e
EdWard680 <ezeward4@gmail.com>
2014-04-08 21:47:17 -0400

    Vector: Brings up to speed with template args

--- Space2D/Vector.h
+++ Space2D/Vector.h
@@ -6,91 +6,140 @@
 namespace jer
 {
 
+template<typename T>
 class Vector {
   protected:
     /// Magnitude of the vector
-    Mag_t Mag;
+    Mag_t<T> Mag;
     
     /// Angle of the vector
-    Radians Theta;
+    Radians<T> Theta;
     
   public:
     /// Create a vector from a magnitude and angle
-    Vector(const Mag_t &mag, const Radians &theta): Mag(mag), Theta(theta) {};
+    Vector(const Mag_t<T> &mag, const Radians<T> &theta): Mag(mag), Theta(theta) {};
     
     /// Create a vector from another vector
-    Vector(Vector const &v2): Mag(v2.Mag), Theta(v2.Theta) {};
+    Vector(Vector<T> const &v2): Mag(v2.Mag), Theta(v2.Theta) {};
     
     /// Create a vector from a Point
-    Vector(const Point &p2);
+    Vector(const Point<T> &p2): Mag(pythagoras(p2)), Theta(getTheta(p2)) {};
     
     /// Create a vector with default values
     Vector(): Mag(1), Theta() {};
     
   public:
     /// Returns the magnitude of the vector
-    const Mag_t &mag() const {
+    const Mag_t<T> &mag() const {
       return Mag;
     };
     
     /// Sets the magnitude of the vector to m
-    virtual Vector& mag(const Mag_t &m);
-    
-    /// Sets the value of the magnitude to m
-    virtual Vector& setMag(const PIXEL_TYPE &m);
+    Vector<T>& mag(const Mag_t<T> &m);
     
     /// Returns the angle of the vector
-    const Radians &theta() const {
+    const Radians<T> &theta() const {
       return Theta;
     };
     
     /// Sets the angle of the vector to t, returns new angle
-    virtual Vector &theta(const Radians &t);
+    Vector<T> &theta(const Radians<T> &t);
     
   public:
     /// Sets the magnitude and angle of the vector to that of another
-    Vector& operator = (Vector const& v2);
+    Vector<T>& operator = (Vector<T> const& v2);
     
     /// Adds the vector to a point
-    Vector& operator += (Point const& p2);
+    Vector<T>& operator += (Point<T> const& p2);
     
     /// Subtracts a point from the vector
-    Vector& operator -= (Point const& p2);
+    Vector<T>& operator -= (Point<T> const& p2);
     
     /// Scales the vector by scale
-    Vector& operator *= (const PIXEL_TYPE &scale);
+    Vector<T>& operator *= (const T &scale);
     
     /// Scales the vector by scale
-    Vector& operator /= (const PIXEL_TYPE &scale);
+    Vector<T>& operator /= (const T &scale);
     
     /// Checks for equality of two vectors
-    const bool operator == (Vector const &v2) const {
+    const bool operator == (Vector<T> const &v2) const {
       return Mag == v2.mag() && Theta == v2.theta();
     };
     
     /// Checks for inequality of two vectors
-    const bool operator != (Vector const &v2) const {
+    const bool operator != (Vector<T> const &v2) const {
       return !(*this == v2);
     };
   };
   
-  inline const Vector& operator * (const Vector& lhs, const PIXEL_TYPE &rhs) {
-    return Vector(lhs) *= rhs;
+  template<typename T>
+  inline Vector<T>& Vector<T>::mag(const Mag_t<T> &m) {
+      Mag = m;
+      return *this;
   }
   
-  inline const Vector& operator * (const PIXEL_TYPE &lhs, const Vector& rhs) {
+  template<typename T>
+  inline Vector<T>& Vector<T>::theta(const Radians<T> &t) {
+      Theta = t;
+      return *this;
+  }
+  
+  template<typename T>
+  inline Vector<T>& Vector<T>::operator= (Vector<T> const &v2) {
+      Mag = v2.mag();
+      Theta = v2.theta();
+      return *this;
+  }
+  
+  template<typename T>
+  inline Vector<T>& Vector<T>::operator+= (Point<T> const &p2) {
+      Mag = pythagoras(p2.x() + getX(*this), p2.y() + getY(*this));
+      Theta = getTheta(p2.x() + getX(*this), p2.y() + getY(*this));
+      return *this;
+  }
+  
+  template<typename T>
+  inline Vector<T>& Vector<T>::operator-= (Point<T> const &p2) {
+      Mag = pythagoras(getX(*this) - p2.x(), getY(*this) - p2.y());
+      Theta = getTheta(getX(*this) - p2.x(), getY(*this) - p2.y());
+      return *this;
+  }
+  
+  template<typename T>
+  inline Vector<T>& Vector<T>::operator*= (const T &scale) {
+      Mag *= scale;
+      return *this;
+  }
+  
+  template<typename T>
+  inline Vector<T>& Vector<T>::operator/= (const T &scale) {
+      Mag /= scale;
+      return *this;
+  }
+  
+  
+  template<typename T>
+  inline const Vector<T>& operator * (const Vector<T>& lhs, const T &rhs) {
+    return Vector<T>(lhs) *= rhs;
+  }
+  
+  template<typename T>
+  inline const Vector<T>& operator * (const T &lhs, const Vector<T>& rhs) {
     return rhs * lhs;
   }
   
-  inline const Vector& operator / (const Vector& lhs, const PIXEL_TYPE &rhs) {
-    return Vector(lhs) /= rhs;
+  template<typename T>
+  inline const Vector<T>& operator / (const Vector<T>& lhs, const T &rhs) {
+    return Vector<T>(lhs) /= rhs;
   }
   
-  inline const PIXEL_TYPE getX(const Vector& v) {
+  template<typename T>
+  inline const X_t<T> getX(const Vector<T>& v) {
     return v.mag() * cos(v.theta());
   }
   
-  inline const PIXEL_TYPE getY(const Vector& v) {
+  template<typename T>
+  inline const Y_t<T> getY(const Vector<T>& v) {
     return v.mag() * sin(v.theta());
   }
 
