1157976a9d070d9d5f7350e11487da48dac25873
EdWard680 <ezeward4@gmail.com>
2014-07-22 13:58:42 -0400

    Changes GameLoop test file to better showcase polymorphic gameloops

--- test/ControlInterfaces/GameLoopTest.cpp
+++ test/ControlInterfaces/GameLoopTest.cpp
@@ -4,7 +4,7 @@
 
 using namespace jer;
 
-class TestLoop: public GameLoop
+class PrimeGeneratorLoop: public GameLoop
 {
     int *primes;
     int current;
@@ -31,7 +31,7 @@ protected:
             }
         }
         
-        if(current == stop)
+        if(current+1 == stop)
             running = false;
         
         return SUCCEEDED;
@@ -39,6 +39,11 @@ protected:
     
     const SUCCESS display() const
     {
+        if(current == 2)
+        {
+            std::cout<<primes[0]<<std::endl;
+            std::cout<<primes[1]<<std::endl;
+        }
         std::cout<<primes[current]<<std::endl;
         return 0;
     }
@@ -69,20 +74,89 @@ protected:
     }
     
 public:
-    TestLoop(const int n): primes(NULL), stop(n) {};
-    virtual ~TestLoop() {};
+    PrimeGeneratorLoop(const int n): primes(NULL), stop(n) {};
+    virtual ~PrimeGeneratorLoop() {};
+};
+
+class FibGeneratorLoop: public GameLoop
+{
+    int *fibs;
+    int current;
+    int stop;
+    
+protected:
+    const SUCCESS loop()
+    {
+        current++;
+        fibs[current] = fibs[current-1] + fibs[current - 2];
+        if(current+1 == stop)
+            running = false;
+        
+        return SUCCEEDED;
+    }
+    
+    const SUCCESS display() const
+    {
+        if(current == 2)
+        {
+            std::cout<<fibs[0]<<std::endl;
+            std::cout<<fibs[1]<<std::endl;
+        }
+        std::cout<<fibs[current]<<std::endl;
+        return 0;
+    }
+    
+    const SUCCESS load()
+    {
+        if(stop <= 2)
+            return -1;
+        
+        if(fibs)
+            unload();
+        
+        fibs = new int[stop];
+        fibs[0] = 1;
+        fibs[1] = 1;
+        current = 1;
+        
+        return 0;
+    }
+    
+    const SUCCESS unload()
+    {
+        if(fibs)
+            delete [] fibs;
+        
+        fibs = NULL;
+        return 0;
+    }
+    
+public:
+    FibGeneratorLoop(const int n): fibs(NULL), stop(n) {};
+    virtual ~FibGeneratorLoop() {};
 };
 
 int main(int argc, char **argv)
 {
-    if(argc < 2)
+    if(argc < 3)
     {
-        std::cout<<"Call with number as first argument"<<std::endl;
+        std::cout<<"Call with number [p or f] as first argument and number as second argument"<<std::endl;
         return 1;
     }
     
-    TestLoop myGameLoop(atoi(argv[1]));
-    return myGameLoop.run();
+    GameLoop *generator = NULL;
+    
+    if(argv[1][0] == 'f')
+        generator = new FibGeneratorLoop(atoi(argv[2]));
+    else if(argv[1][0] == 'p')
+        generator = new PrimeGeneratorLoop(atoi(argv[2]));
+    else
+    {
+        std::cout<<"First arg should be either f (fibinnaci) or p (prime)"<<std::endl;
+        return 1;
+    }
+    
+    return generator->run();
 }
 
 
