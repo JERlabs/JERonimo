7fa6bec7b6a748da830ebdb0fc93076a0911acee
EdWard680 <ezeward4@gmail.com>
2014-07-22 03:38:19 -0400

    Removes virtual function call from destructor

--- ControlInterfaces/Loadable.h
+++ ControlInterfaces/Loadable.h
@@ -10,13 +10,16 @@ namespace jer
     public:
         virtual ~Loadable()
         {
+/*
 #ifndef DEBUG
             unload();
 #else
             if(unload() < 0)
                 std::cerr<<"Loadable::unload("<<this<<") returned failure"<<std::endl;
 #endif
+*/
         };
+
         virtual const SUCCESS load()=0;  /// Overload to load object
         virtual const SUCCESS unload()=0;  /// Overload to clean up object
     };
