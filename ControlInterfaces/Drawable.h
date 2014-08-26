9733711dfa6ec17bd5e4004c5f5f607d46d201a8
EdWard680 <ezeward4@gmail.com>
2014-08-26 03:24:02 -0400

    hrm

---
+++ ControlInterfaces/Drawable.h
@@ -0,0 +1,17 @@
+#ifndef _DRAWABLE_H_
+#define _DRAWABLE_H_
+
+#include "Declarations.h"
+
+namespace jer
+{
+    class Drawable
+    {
+    public:
+        virtual drawLine(const Point<int>& p1, const Point<int>& p2)=0;
+        virtual drawLines(
+    };
+}
+
+
+#endif /*_DRAWABLE_H_*/
