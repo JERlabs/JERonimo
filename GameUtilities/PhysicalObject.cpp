8ffb5c2e47e59a93e7405b3e92d25e363a83e111
EdWard680 <ezeward4@gmail.com>
2014-09-09 15:11:40 -0400

    Eliminates gravity when objects collide

--- GameUtilities/PhysicalObject.cpp
+++ GameUtilities/PhysicalObject.cpp
@@ -3,7 +3,7 @@
 namespace jer
 {
     double Mass::GRAVITATIONAL_CONSTANT(0.1);
-    double PhysicalObject::REST_THRESHOLD(0.1);
+    double PhysicalObject::REST_THRESHOLD(0.05);
     
     const Scalar<double> Mass::GetGravityForce(const Mass &first, const Mass &second, const Scalar<double> &distance)
     {
@@ -14,6 +14,12 @@ namespace jer
     {
         Vector distance(getPosition()-other.getPosition());
         force(Vector(Mag_t<double>(-Mass::GetGravityForce(getMass(), other.getMass(), distance.mag())/getMass()), distance.theta()));
+    }
+	
+	void PhysicalObject::antigravitate(const PhysicalObject &other)
+	{
+        Vector distance(getPosition()-other.getPosition());
+        force(Vector(Mag_t<double>(Mass::GetGravityForce(getMass(), other.getMass(), distance.mag())/getMass()), distance.theta()));
     }
     
     const SUCCESS PhysicalObject::loop()
@@ -29,24 +35,24 @@ namespace jer
         Mag_t<double> initVel(initVec.mag());
         Radians initTheta(initVec.theta());
         
+		if(fabs(initVel) < REST_THRESHOLD)
+		{
+			setVelocity(getVelocity()+initVec/2.0);
+			object.setVelocity(object.getVelocity()-initVec/2.0);
+			antigravitate(object);
+			object.antigravitate(*this);
+			return SUCCEEDED;
+		}
+		
         // Frame of reference with the object at rest with this approaching it with an angle of 0
         Point<double> p2(Vector(Mag_t<double>(2.0*getMass()*getX(initVel, initTheta+angle)/(getMass()+object.getMass())), initTheta+angle));  // Gets the velocity of 2 based on elastic collision equation
         Point<double> p1((initVel*getMass()-p2.x()*object.getMass())/getMass(), -p2.y()*object.getMass()/getMass());   // Gets the velocity of 1 based off of elastic collision rulez
-        
-        if(abs(p1.x()) < REST_THRESHOLD && abs(p2.x()) < REST_THRESHOLD)
-        {
-            p1.x(0.0);
-            p2.x(0.0);
-        }
         
         Vector v1(p1);
         Vector v2(p2);
         
         v1.theta(-v1.theta()+initTheta);
         v2.theta(angle);
-        
-        v1 += object.getVelocity();
-        v2 += object.getVelocity();
         
         Point<double> dif(object.getPosition()-*position);
         while(getCollider()->collides(*object.getCollider()))
