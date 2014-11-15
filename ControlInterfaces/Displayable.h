b28a9699c4750f743099c68998936b5811e8113c
EdWard680 <ezeward4@gmail.com>
2014-11-15 03:57:52 -0500

    Adds documentation to Control Interface base classes

--- ControlInterfaces/Displayable.h
+++ ControlInterfaces/Displayable.h
@@ -7,25 +7,57 @@
 namespace jer
 {
     using std::shared_ptr;
+	
+	/** Abstract class for interface of displayed game elements.
+	 * These objects should have a routine which is meant to be run every frame, without conceptually changing internal data.
+	 * One should derive a Displayable and override the display function,
+	 * then pass that object into a list of Displayable references to be once every frame
+	 * \see Loopable
+	 * \see Loadable
+	 * \see DisplayWrapper
+	 * \see DisplayQueue
+	 */
     class Displayable
     {        
     public:
+		/// Destructor
         virtual ~Displayable() {};
+		
+		/// Default Constructor
         Displayable() {};
         
     public:
+		/** This function should be overridden in a derived class with the routine which runs once per frame.
+		 * It's called every frame by DisplayQueue's.
+		 * Function is constant, and shouldn't change any conceptual internal data.
+		 * \return status code defined by derived class. Negative on failure, non-negative on success
+		 */
         virtual const SUCCESS display() const=0;   // We'll see about this const rule.
     };
     
+	/** Wrapper class for memory management of Displayable
+	 * All Displayable's should be allocated dynamically.
+	 */
     class DisplayWrapper: public shared_ptr<Displayable>, public Displayable
     {
     public:
+		/// Destructor, shared_ptr ensures proper clean up of Displayable
         virtual ~DisplayWrapper() {};
+		
+		/// (Default) Construct from pointer to dynamically allocated Displayable object.
         DisplayWrapper(Displayable * const comp=nullptr): shared_ptr<Displayable>(comp) {};
+		
+		/// Construct from shared_ptr of dynamically allocated Displayable object.
         DisplayWrapper(const shared_ptr<Displayable>& other): shared_ptr<Displayable>(other) {};
+		
+		/// Copy Constructor
         DisplayWrapper(const DisplayWrapper& other): shared_ptr<Displayable>(other) {};
         
     public:
+		/** Calls the Loadable object being managed's display function
+		 * \return the return value of the managed object's display, or jer::FAILED if no object is being managed.
+		 * \see Displayable::display
+		 */
         virtual const SUCCESS display() const override {return get()? get()->display():-1;};
     };
 
--- ControlInterfaces/Loadable.h
+++ ControlInterfaces/Loadable.h
@@ -9,19 +9,45 @@ namespace jer
 {
     using std::shared_ptr;
     
-    
+    /** Abstract class for interface of loaded game elements.
+	 * These objects should have a routine which is meant to be run before the resources use, and after, safely loading the resource into memory, and cleaning the resource from memory.
+	 * One should derive a Loadable and override the load and unload functions,
+	 * then pass that object into a list of Loadable references to be executed before and after the resources are used
+	 * \see Displayable
+	 * \see Loopable
+	 * \see LoadWrapper
+	 * \see DataManager
+	 */
     class Loadable
     {
     private:
-        bool loaded;
+        bool loaded;  ///< True if the memory is currently loaded into memory. False otherwise.
         
     public:
+		/// Default constructor
         Loadable(): loaded(false) {};
+		
+		/// Copy constructor
         Loadable(const Loadable& other): loaded(other.loaded) {};
+		
+		/// Destructor
         virtual ~Loadable() {};
-
-        virtual const SUCCESS load()=0;  /// Overload to load object
-        virtual const SUCCESS unload()=0;  /// Overload to clean up object
+		
+		/** This function should be overriden in a derived class with a routine which loads in a resource from memory.
+		 * The default implementation provided sets loaded to true and returns jer::SUCCEEDED.
+		 * \return status code defined by derived class. Negative on failure code should also guarantee that loaded is false. Non-negative on success should also imply that loaded is true.
+		 * \see Loadable::unload
+		 */
+        virtual const SUCCESS load()=0;
+		
+		/** This function should be overriden in a derived class with a routine which ensures the resources is cleared from memory and freed.
+		 * The default implementation provided sets loaded to false and returns jer::SUCCEEDED.
+		 * \return status code defined by derived class. Negative on failure code, Non-negative on success. Regardless it's assumed that the resource can no longer be utilized and loaded should be false.
+		 * \see Loadable::load
+		 */
+        virtual const SUCCESS unload()=0;
+		
+		/// Returns true if resource is loaded and ready to be utilized, false otherwise
         const bool isLoaded() const {return loaded;};  /// Used to check the state of the loadable object
     };
     
@@ -37,24 +63,49 @@ namespace jer
         return SUCCEEDED;
     }
     
+	/** Wrapper class for memory management of Loadable
+	 * All Loadable's should be allocated dynamically.
+	 *//** Wrapper class for memory management of Loadable
+	 * All Loadable's should be allocated dynamically.
+	 */
     class LoadWrapper: public shared_ptr<Loadable>, public Loadable
     {
     public:
+		/// Destructor. Unloads resource before the Loadable is deleted.
         virtual ~LoadWrapper() {if(get()->isLoaded()) get()->unload();};
+		
+		/// (Default) Construct from pointer to dynamically allocated Loadable object
         LoadWrapper(Loadable * const comp=nullptr): shared_ptr<Loadable>(comp) {};
+		
+		/// Construct from shared_ptr managing dynamically allocated Loadable object
         LoadWrapper(const shared_ptr<Loadable>& other): shared_ptr<Loadable>(other) {};
+		
+		/// Copy Constructor
         LoadWrapper(const LoadWrapper& other): shared_ptr<Loadable>(other), Loadable(other) {};
         
     public:
+		/** Calls the Loadable object being managed's load function
+		 * \return the return value of the managed object's load, or jer::FAILED if no object is being managed.
+		 * \see LoadWrapper::unload
+		 * \see Loadable::load
+		 */
         virtual const SUCCESS load() override 
         {
             if(get())
             {
-                Loadable::load();
-                return get()->load();
+				const ret = get()->load();
+				if(ret >= SUCCEEDED)
+					Loadable::load();
+                return ret;
             }
             return FAILED;
         };
+		
+		/** Calls the Loadable object being managed's unload function
+		 * \return the return value of the managed object's unload, or jer::FAILED if no object is being managed.
+		 * \see LoadWrapper::load
+		 * \see Loadable::unload
+		 */
         virtual const SUCCESS unload() override 
         {
             Loadable::unload();
